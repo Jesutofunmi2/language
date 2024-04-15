5f14f93b09d457cb6d680507d59bf038229e3f80
Balogun <balogunsunday91@gmail.com>
2024-04-15 12:33:25 +0100

    auth page

--- app/(marketing)/header.tsx
+++ app/(marketing)/header.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import Image from 'next/image';
+import { ClerkLoaded, ClerkLoading, SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs';
+import { Loader } from 'lucide-react';
+import { Button } from '@/components/ui/button';
 
 export const Header = () => {
   return (
@@ -7,7 +10,27 @@ export const Header = () => {
         <div className='lg:max-w-screen-lg mx-auto flex items-center justify-between h-full'>
             <div className='pt-8 pl-4 pb-7 flex items-center gap-x-3'>
                 <Image src="/mascot.svg" alt='Mascot' height={40} width={40}/>
+                <h1 className='text-green-600 text-2xl font-extrabold tracking-wide '>Patois</h1>
             </div>
+            <ClerkLoading>
+                <Loader className='h-5 w-5 text-muted-foreground animate-spin' />
+            </ClerkLoading>
+            <ClerkLoaded>
+                <SignedIn>
+                    <UserButton  afterSignOutUrl="/"/>
+                </SignedIn>
+                <SignedOut>
+                    <SignInButton 
+                       mode="modal"
+                       afterSignInUrl="/learn"
+                       afterSignUpUrl="/learn"
+                       > 
+                        <Button variant="ghost" size="lg">
+                            Login
+                        </Button>
+                    </SignInButton>
+                </SignedOut>
+            </ClerkLoaded>
         </div>
     </header>
   )
