7af8a88c5167ca45f02928ff9655d12b4f2ac4f8
Balogun <balogunsunday91@gmail.com>
2024-10-24 12:28:17 +0100

    list of courses

--- app/(main)/courses/card.tsx
+++ app/(main)/courses/card.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 
 type Props = {
   title: string;
-  id: number;
+  id?: number;
   imageSrc: string;
   onClick: (id: number) => void;
   disabled?: boolean;
@@ -22,7 +22,7 @@ export const Card = ({
 }: Props) => {
   return (
     <div
-      onClick={() => onClick(id)}
+      //onClick={() => onClick(id)}
       className={cn(
         "h-full border-2 rounded-xl border-b-4 hover:bg-black/5 cursor-pointer active:border-b-2 flex flex-col items-center justify-between p-3 pb-6 min-h-[217px] min-w-[200px]",
         disabled && "pointer-events-none opacity-50"
